f3525a9de073a52d8b9b90100ae3d19d03e70c86
Sulav <sulavaryal@gmail.com>
2018-10-16 10:02:49 +0545

    added access to asset's css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{config('app.name','LSAPP')}}</title>
+        <!--This gives us access to assets-->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>{{config('app.name','LSAPP')}}</title>
     </head>
     <body>
        @yield('content')
