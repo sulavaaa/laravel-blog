be39a12077626975efdce7d4bcf01d438f9e7fc8
Sulav <sulavaryal@gmail.com>
2018-10-16 09:16:54 +0545

    passing value into view #second and recommended method.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,20 +8,22 @@ class PagesController extends Controller
 {
     public function index(){
         //Passing Value into blade template
-        $title = 'Hello Laravel';
+        $title = 'Hello Laravel!';
         //return view('pages.index');
         // 2 ways to do it. 
-        // First way
-        return view('pages.index',compact('title'));
+        // Second way : this way we can pass multiple values and thus is recommended way
+        return view('pages.index')->with('title',$title);
 
     }
 
     public function about(){
-        return view('pages.about');
+        $title = 'Hello About page!!';
+        return view('pages.about')->with('title',$title);
     }
 
     public function services(){
-        return view('pages.services');
+        $title = 'Hello Services!';
+        return view('pages.services')->with('title',$title);
     }
 
 }
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1>Services</h1>
+        <h1>{{$title}}</h1>
         <p>This is the services page.</p>
 @endsection
