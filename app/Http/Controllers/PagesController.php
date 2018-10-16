280acf8e42b60611475faea64ef3576ccb68ef7c
Sulav <sulavaryal@gmail.com>
2018-10-16 09:06:08 +0545

    passing value into blade template #method 1

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        //Passing Value into blade template
+        $title = 'Hello Laravel';
+        //return view('pages.index');
+        // 2 ways to do it. 
+        // First way
+        return view('pages.index',compact('title'));
+
     }
 
     public function about(){
