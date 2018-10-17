1018b2dc85d409b70b2b32eae52b2d497455039c
Sulav <sulavaryal@gmail.com>
2018-10-17 19:00:15 +0545

    added create form

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -4,19 +4,23 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/services">Services</a>
-        </li> 
-        <li class="nav-item">
-            <a class="nav-link" href="/posts">Blog</a>
-        </li>    
+        <ul class="navbar-nav navbar-right">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/services">Services</a>
+            </li> 
+            <li class="nav-item">
+                <a class="nav-link" href="/posts">Blog</a>
+            </li>    
         </ul>
-    </div>  
+        <ul class="navbar-nav ml-auto">
+            <li><a href="/posts/create" class="btn btn-md btn-primary">Create New</a></li>
+        </ul>
+    </div>
+    
 </nav>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,14 +4,14 @@
     
     <hr>
     <h1>Create Post</h1>
-    {!! Form::open(['action' => 'PostsController@store','method'=>'POST']) !!}
+    {!! Form::open(["action" => "PostsController@store", "method" => "POST", "class" => "form"]) !!}
         <div class="form-group">
-            {{Form::label('title', 'Title of the Post')}}
-            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+                {{Form::label('title', 'Title')}}
+                {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
         </div>
         <div class="form-group">
             {{Form::label('body', 'Body')}}
-            {{Form::textarea('title', '', ['class' => 'form-control', 'placeholder' => 'Add text'])}}
+            {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Add text'])}}
         </div>
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
